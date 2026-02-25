2b4337ddc52e9fda6941e8eeb54adb60f46cff32
aattifah teladia <aattifahteladia@aattifahs-MacBook-Air.local>
2026-02-25 19:13:37 +0000

    Add download page

---
+++ app/download/page.tsx
@@ -0,0 +1,8 @@
+export default function DownloadPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Your Printable Pack</h1>
+      <p>Thanks for your purchase. Your downloads will be here.</p>
+    </main>
+  );
+}
